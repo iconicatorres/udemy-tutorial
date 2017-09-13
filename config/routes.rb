74fe77e00a8c4e558a1e9b104048f1502453d5ca
Agustin Torres <agustintorres@MacBook-Pro-de-Agustin.local>
2017-09-13 17:55:41 -0500

    Rutas customizadas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  get 'porfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   #get 'pages/home'
   get 'about', to: 'pages#about'
